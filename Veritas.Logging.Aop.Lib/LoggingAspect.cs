03a4e182cbb514908d65bc4370c7c8062a970d7f
WorkComputer <josh.d.kofoot@rrd.com>
2017-10-16 21:30:23 -0500

    Inital checking of code

---
+++ Veritas.Logging.Aop.Lib/LoggingAspect.cs
@@ -0,0 +1,52 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Veritas.Logging.Aop.Lib
+{
+    [Serializable]
+    public class LoggingAspect : OnMethodBoundaryAspect
+    {
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            var stackTrace = new StackTrace().GetFrame(1);
+
+            LogManager.ReconfigExistingLoggers();
+            var log = LogManager.GetLogger(stackTrace.GetType().Name);
+            log.Debug(string.Format("{0} started with parms {1}", stackTrace.GetMethod().Name, string.Join(", ",args.Arguments)));
+
+            base.OnEntry(args);
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            var stackTrace = new StackTrace().GetFrame(1);
+            
+            LogManager.ReconfigExistingLoggers();
+            var log = LogManager.GetLogger(stackTrace.GetType().Name);
+            log.Debug(string.Format("{0} ended.", stackTrace.GetMethod().Name));
+
+            base.OnExit(args);
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            var stackTrace = new StackTrace().GetFrame(1);
+
+            LogManager.ReconfigExistingLoggers();
+            var log = LogManager.GetLogger(stackTrace.GetType().Name);
+            log.Error(args.Exception, string.Format("{0} Errored. Message {1}", stackTrace.GetMethod().Name, args.Exception.Message));
+
+            base.OnException(args);
+        }
+    }
+}
---
+++ Veritas.Logging.Aop.Lib/TimingAspect.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veritas.Logging.Aop.Lib
+{
+
+    [Serializable]
+    [MulticastAttributeUsage(MulticastTargets.Method)]
+    public class TimingAspect : OnMethodBoundaryAspect
+    {
+        [NonSerialized]
+        Stopwatch _stopwatch;
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            _stopwatch = Stopwatch.StartNew();
+            base.OnEntry(args);
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            var stackTrace = new StackTrace().GetFrame(1);
+
+            LogManager.ReconfigExistingLoggers();
+            var log = LogManager.GetLogger(stackTrace.GetType().Name);
+            log.Debug(string.Format("{0} took {1}ms to execute", new StackTrace().GetFrame(1).GetMethod().Name, _stopwatch.ElapsedMilliseconds));
+            
+            base.OnExit(args);
+        }
+    }
+}
